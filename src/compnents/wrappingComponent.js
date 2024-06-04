a31fe9c84fd38dc70a421e7802e1705ef8c1b7ce
oriSpotower <ori@spotower.com>
2024-06-04 22:43:13 +0300

    .

--- src/compnents/wrappingComponent.js
+++ src/compnents/wrappingComponent.js
@@ -1,12 +1,13 @@
 const WrappingComponent = ({ children, pagePropsFrom_App }) => {
   //   console.log("pagePropsFrom_App: ", pagePropsFrom_App);
   //   if (!pagePropsFrom_App || pagePropsFrom_App == {})
-  if (false) {
-    console.log(
-      "WrappingComponent.js , pagePropsFrom_App",
-      pagePropsFrom_App.secret1.innerSecret1
-    );
-  }
+  console.log("typeof pagePropsFrom_App: ", typeof pagePropsFrom_App);
+  //   if (false) {
+  //     console.log(
+  //       "WrappingComponent.js , pagePropsFrom_App",
+  //       pagePropsFrom_App.secret1.innerSecret1
+  //     );
+  //   }
 
   return <div>{children}</div>;
 };
