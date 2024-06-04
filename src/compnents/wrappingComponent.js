06d26c6707fb2997017c3abeb3e08e87568022f8
oriSpotower <ori@spotower.com>
2024-06-04 22:39:00 +0300

    .

--- src/compnents/wrappingComponent.js
+++ src/compnents/wrappingComponent.js
@@ -3,10 +3,10 @@ const WrappingComponent = ({ children, pagePropsFrom_App }) => {
   //   console.log("WrappingComponent.js , pagePropsFrom_App", pagePropsFrom_App);
   console.log("pagePropsFrom_App: ", pagePropsFrom_App);
   if (!pagePropsFrom_App || pagePropsFrom_App == {}) return;
-  console.log(
-    "WrappingComponent.js , pagePropsFrom_App",
-    pagePropsFrom_App.secret1.innerSecret1
-  );
+  //   console.log(
+  //     "WrappingComponent.js , pagePropsFrom_App",
+  //     pagePropsFrom_App.secret1.innerSecret1
+  //   );
   return <div>{children}</div>;
 };
 
