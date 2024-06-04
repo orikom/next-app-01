98cc2bab9e3b9f3782dafeb63752f84955d3a8d9
oriSpotower <ori@spotower.com>
2024-06-04 22:29:13 +0300

    .

--- src/compnents/wrappingComponent.js
+++ src/compnents/wrappingComponent.js
@@ -1,6 +1,7 @@
 const WrappingComponent = ({ children, pagePropsFrom_App }) => {
   // this is working because no dot operator
   //   console.log("WrappingComponent.js , pagePropsFrom_App", pagePropsFrom_App);
+  if (!pagePropsFrom_App) return;
   console.log(
     "WrappingComponent.js , pagePropsFrom_App",
     pagePropsFrom_App.secret1.innerSecret1
