a6c00b584d7e68822c6585f91a1862ca05728bf9
oriSpotower <ori@spotower.com>
2024-06-04 22:53:11 +0300

    .

--- src/compnents/wrappingComponent.js
+++ src/compnents/wrappingComponent.js
@@ -1,13 +1,23 @@
 const WrappingComponent = ({ children, pagePropsFrom_App }) => {
-  //   console.log("pagePropsFrom_App: ", pagePropsFrom_App);
-  //   if (!pagePropsFrom_App || pagePropsFrom_App == {})
-  console.log("typeof pagePropsFrom_App: ", typeof pagePropsFrom_App);
-  //   if (false) {
-  //     console.log(
-  //       "WrappingComponent.js , pagePropsFrom_App",
-  //       pagePropsFrom_App.secret1.innerSecret1
-  //     );
-  //   }
+  console.log("pagePropsFrom_App: ", pagePropsFrom_App);
+
+  if (JSON.stringify(pagePropsFrom_App) === JSON.stringify({})) {
+    console.log("pagePropsFrom_App is an empty object");
+    return;
+  } else {
+    console.log("pagePropsFrom_App is not an empty object");
+  }
+  //   console.log("typeof pagePropsFrom_App: ", pagePropsFrom_App);
+
+  console.log(
+    "WrappingComponent.js , pagePropsFrom_App",
+    pagePropsFrom_App.secret1
+  );
+
+  //   console.log(
+  //     "WrappingComponent.js , pagePropsFrom_App",
+  //     pagePropsFrom_App.secret1.innerSecret1
+  //   );
 
   return <div>{children}</div>;
 };
